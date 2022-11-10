a110dfe06901a28c8777c12c64afc5548be04baf
dveer <dveer@192.168.1.9>
2022-11-10 14:16:24 -0600

    push

--- project_1/src/student/assignment1.java
+++ project_1/src/student/assignment1.java
@@ -1,10 +1,17 @@
 package student;
 
 public class assignment1 {
-	
-//comment
-//comment
-	
-	
+
+	public void sum(int a,int b)
+	{
+		int c;
+		c=a+b;
+		System.out.println("sum of two numbers:"+c);
+	}	
+public static void main(String[] args) {
+assignment1 ref=new assignment1();
+    ref.sum(9,4);
+        
+}	
 	
 }
